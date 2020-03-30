0aa184ff19b0ca8cb1971bdecd386092997c65ab
wzdnn <62141495+wzdnn@users.noreply.github.com>
2020-03-30 23:29:22 +0700

    method pembagian

---
+++ pembagian github.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void pembagian ()
+{
+	float a,b;
+	
+	cout << "Masukkan angka Pertama : ";
+	cin  >> a;
+	
+	cout << "Masukkan angka Kedua   : ";
+	cin  >> b;
+	
+	
+	cout << "Hasil Pembagian : " << a/b;
+}
+
+main ()
+{
+	pembagian();
+}
